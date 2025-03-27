5b420aaed85b65ecb7aba7eec9651dd938d1860d
vynguyen1242 <131448946+vynguyen1242@users.noreply.github.com>
2025-03-27 22:01:48 +0700

    Update NhomTheLoaiBUS.java

--- src/main/java/BUS/NhomTheLoaiBUS.java
+++ src/main/java/BUS/NhomTheLoaiBUS.java
@@ -1,5 +1,36 @@
 package BUS;
 
+import DTO.NhomTheLoaiDTO;
+import DAO.NhomTheLoaiDAO;
+import java.util.ArrayList;
+/**
+ *
+ * @author Thanh tam
+ */
 public class NhomTheLoaiBUS {
-    
+    private final NhomTheLoaiDAO NhomTheLoaiDAO;
+
+    public NhomTheLoaiBUS() {
+        NhomTheLoaiDAO = new NhomTheLoaiDAO();
+    }
+
+    // Retrieve all author groups
+    public ArrayList<NhomTheLoaiDTO> getAllTL() {
+        return NhomTheLoaiDAO.getAll();
+    }
+
+    // Add a new author group
+    public boolean addNTL(NhomTheLoaiDTO ntl) {
+        return NhomTheLoaiDAO.add(ntl) > 0;
+    }
+
+    // Update an existing author group
+    public boolean updateNTL(NhomTheLoaiDTO ntl) {
+        return NhomTheLoaiDAO.update(ntl) > 0;
+    }
+
+    // Delete an author group
+    public boolean deleteNTL(int MaTL) {
+        return NhomTheLoaiDAO.delete(MaTL) > 0;
+    }
 }
