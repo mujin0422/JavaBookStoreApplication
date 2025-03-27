8551d0f181373484843ab827b8776cf948784f04
vynguyen1242 <131448946+vynguyen1242@users.noreply.github.com>
2025-03-27 16:11:36 +0700

    Update NhomTacGiaBUS.java

--- src/main/java/BUS/NhomTacGiaBUS.java
+++ src/main/java/BUS/NhomTacGiaBUS.java
@@ -1,5 +1,33 @@
 package BUS;
 
+import DTO.NhomTacGiaDTO;
+import DAO.NhomTacGiaDAO;
+import java.util.ArrayList;
+
 public class NhomTacGiaBUS {
-    
+    private final NhomTacGiaDAO nhomTacGiaDAO;
+
+    public NhomTacGiaBUS() {
+        nhomTacGiaDAO = new NhomTacGiaDAO();
+    }
+
+    // Retrieve all author groups
+    public ArrayList<NhomTacGiaDTO> getAllTG() {
+        return nhomTacGiaDAO.getAll();
+    }
+
+    // Add a new author group
+    public boolean addTG(NhomTacGiaDTO ntg) {
+        return nhomTacGiaDAO.add(ntg) > 0;
+    }
+
+    // Update an existing author group
+    public boolean updateNTG(NhomTacGiaDTO ntg) {
+        return nhomTacGiaDAO.update(ntg) > 0;
+    }
+
+    // Delete an author group
+    public boolean deleteNTG(int maTG) {
+        return nhomTacGiaDAO.deleteTG(maTG) > 0;
+    }
 }
