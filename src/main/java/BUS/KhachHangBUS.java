8e8d496e63b5cc2e3f0de784d1dade12fceff5b8
Tester <110340414+Baothanhtam@users.noreply.github.com>
2025-04-18 21:38:07 +0700

    KhachHangBUS.java

--- src/main/java/BUS/KhachHangBUS.java
+++ src/main/java/BUS/KhachHangBUS.java
@@ -1,28 +1,24 @@
 package BUS;
-
 import DAO.KhachHangDAO;
 import DTO.KhachHangDTO;
 import java.util.ArrayList;
-
+import java.util.Date;
+import java.util.List;
 public class KhachHangBUS {
     private KhachHangDAO khachHangDAO;
-
-    public KhachHangBUS() {
+   
+ public KhachHangBUS() {
         khachHangDAO = new KhachHangDAO();
     }
-
     public ArrayList<KhachHangDTO> getAllKhachHang() {
         return khachHangDAO.getAll();
     }
-
     public boolean addKhachHang(KhachHangDTO khachHang) {
         return khachHangDAO.add(khachHang) > 0;
     }
-
     public boolean updateKhachHang(KhachHangDTO khachHang) {
         return khachHangDAO.update(khachHang) > 0;
     }
-
     public boolean deleteKhachHang(int maKH) {
         return khachHangDAO.delete(maKH) > 0;
     }
@@ -50,4 +46,21 @@ public class KhachHangBUS {
         }
         return ketQua;
     }
+    
+     public List<Object[]> getTopKhachMuaNhieu() {
+        return khachHangDAO.getTopKhachMuaNhieu();
+    }
+
+    public List<Object[]> getKhachHangTheoNgay(Date fromDate, Date toDate) {
+        return khachHangDAO.thongKeKhachHangTheoNgay(fromDate, toDate);
+    }
+    public List<Object[]> getKhachHangTheoThang(String monthYear) { 
+        return khachHangDAO.getKhachHangTheoThang(monthYear);
+    }
+    public List<Object[]> getDanhSachKhachDaMua() {
+    return khachHangDAO.getDanhSachKhachDaMua();
+    }
 }
+
+
+
