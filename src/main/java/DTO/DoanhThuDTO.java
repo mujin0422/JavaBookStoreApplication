fde5f4f8378cbc362837934bfa2812acfc3aaef1
vynguyen1242 <131448946+vynguyen1242@users.noreply.github.com>
2025-04-21 11:46:49 +0700

    Create DoanhThuDTO.java

---
+++ src/main/java/DTO/DoanhThuDTO.java
@@ -0,0 +1,80 @@
+package DTO;
+
+import java.util.Date;
+
+public class DoanhThuDTO {
+    private int maSach;
+    private String tenSach;
+    private int maKH;
+    private String tenKH;
+    private Date ngayXuat;
+    private int soLuong;
+    private double giaBan;
+    private double tongTien;
+    
+  
+
+    public int getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(int maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTenSach() {
+        return tenSach;
+    }
+
+    public void setTenSach(String tenSach) {
+        this.tenSach = tenSach;
+    }
+
+    public int getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(int maKH) {
+        this.maKH = maKH;
+    }
+
+    public String getTenKH() {
+        return tenKH;
+    }
+
+    public void setTenKH(String tenKH) {
+        this.tenKH = tenKH;
+    }
+
+    public Date getNgayXuat() {
+        return ngayXuat;
+    }
+
+    public void setNgayXuat(Date ngayXuat) {
+        this.ngayXuat = ngayXuat;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public double getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(double giaBan) {
+        this.giaBan = giaBan;
+    }
+
+    public double getTongTien() {
+        return tongTien;
+    }
+
+    public void setTongTien(double tongTien) {
+        this.tongTien = tongTien;
+    }
+}
